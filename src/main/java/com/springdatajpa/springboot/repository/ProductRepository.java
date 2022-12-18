da329531a85a79bbe1e8d7a2cf9c1259a3f9cf94
shankar87dayal <shankar.87.dayal@gmail.com>
2022-12-18 07:37:48 +0530

    create query method in productRepository class and also test this method by creating one more class QueryMethodsTest

--- src/main/java/com/springdatajpa/springboot/repository/ProductRepository.java
+++ src/main/java/com/springdatajpa/springboot/repository/ProductRepository.java
@@ -3,5 +3,23 @@ package com.springdatajpa.springboot.repository;
 import com.springdatajpa.springboot.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product,Long> {
+
+    /*
+    * Returns the found product entry by using its name as search
+    * criteria. if no product entry is found, this method
+    * returns null.
+    * */
+    public Product findByName(String name);
+
+
+    /**
+    * Return an optional which contains the found product
+     * entry by using its id as search criteria. if no product entry
+     * is found, this method returns an empty optional.
+     *
+     * */
+    Optional<Product> findById(Long id);
 }
---
+++ src/test/java/com/springdatajpa/springboot/repository/QueryMethodsTest.java
@@ -0,0 +1,31 @@
+package com.springdatajpa.springboot.repository;
+
+import com.springdatajpa.springboot.entity.Product;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class QueryMethodsTest {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    void findByNameMethod(){
+        Product product = productRepository.findByName("product1");
+
+        System.out.println(product.getId());
+        System.out.println(product.getName());
+        System.out.println(product.getDescription());
+    }
+
+    @Test
+    void findByIdMethod(){
+        Product product = productRepository.findById(2L).get();
+
+        System.out.println(product.getId());
+        System.out.println(product.getName());
+        System.out.println(product.getDescription());
+    }
+}
