4fc2a926a2661dd77476456f6e7c066265d034d8
shankar87dayal <shankar.87.dayal@gmail.com>
2022-12-24 06:40:01 +0530

    Create order and address class and it's repository interface and mapped these by @One to One mapping here source is order class and target is address class

---
+++ src/main/java/com/springdatajpa/springboot/entity/Address.java
@@ -0,0 +1,21 @@
+package com.springdatajpa.springboot.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "addresses")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String street;
+    private String city;
+    private String state;
+    private String country;
+    private String zipCode;
+
+
+}
---
+++ src/main/java/com/springdatajpa/springboot/entity/Order.java
@@ -0,0 +1,36 @@
+package com.springdatajpa.springboot.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+/*@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString*/ // you can use @Data annotation from lombok it is internally provide all these annotations.
+@Data
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String orderTrackingNumber;
+    private int totalQuentity;
+    private BigDecimal totalPrice;
+    private String status;
+    @CreationTimestamp
+    private LocalDateTime orderCreated;
+
+    @UpdateTimestamp
+    private LocalDateTime lastUpdated;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "billing_address_id", referencedColumnName = "id")
+    private Address billingAddress;
+}
